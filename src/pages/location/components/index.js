48511fd6e068c20eb93a7e37dacc99efd6fe3aa9
John <johnelvis963@gmail.com>
2024-10-26 16:29:22 -0500

    index para ahorrar las importaciones en linea

---
+++ src/pages/location/components/index.js
@@ -0,0 +1,8 @@
+export * from './card/CardLocal'; 
+export * from './card/Locals'; 
+export * from './filtro/FiltroLocal'; 
+export * from './input/InputSearch'; 
+export * from '@/components/texts/TitleCustom'; 
+export * from '@/components/boton/ButtomCuston'; 
+export { HeroIconRayo } from '../../../components/common/HeroIconRayo';
+export { CardSkeleton } from "../../../components/cards/CardSkeletons";
---
+++ src/pages/locationDetail/components/index.js
@@ -0,0 +1,10 @@
+export * from './LocalCarrusel';
+export * from './LinearAndText';
+export * from './ListInformation';
+
+export { TitleCustom } from "../../../components/texts/TitleCustom";
+export { CardSkeleton } from "../../../components/cards/CardSkeletons";
+export { HeroIconRayo } from "../../../components/common/HeroIconRayo";
+export { ErrorComponents } from '../../../components/error/ErrorComponents'
+export { ButtomCuston } from "../../../components/boton/ButtomCuston";
+export { CustomSliderGeneral } from "../../../components/slider/CustomSliderGeneral";
